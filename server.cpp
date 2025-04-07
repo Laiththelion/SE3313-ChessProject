d138ea97741fea221a4bc7c0e488594f427bfa16
laith1 <66022904+Laiththelion@users.noreply.github.com>
2025-04-07 15:53:29 -0400

    Update server.cpp
    finalized graceful process and thread handling

--- server.cpp
+++ server.cpp
@@ -1,150 +1,116 @@
 #include <iostream>
-#include <unistd.h>
-#include <string>
-#include <cstring>
 #include <thread>
-#include <mutex>
 #include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <csignal>
 #include <sys/wait.h>
-#include <signal.h>
+#include <mutex>
 #include <semaphore.h>
 
 #define PORT 8080
 
-std::mutex cout_mutex;
+std::vector<pid_t> child_pids;
+std::mutex pid_mutex;
+sem_t connection_sem;
 
-sem_t sem_white, sem_black;
-
-void handle_player(int player_socket, int opponent_socket, std::string color) {
-    char buffer[1024];
-
-    // Debug info: process and thread ID
-    std::cout << "[Match Thread][" << color << "] PID: " << getpid()
-              << " | TID: " << std::this_thread::get_id() << std::endl;
-
-    // Send role to player
-    std::string role_msg = "ROLE:" + color;
-    send(player_socket, role_msg.c_str(), role_msg.length(), 0);
-
-    while (true) {
-        if (color == "WHITE") {
-            sem_wait(&sem_white);  // WHITE waits for turn
-        } else {
-            sem_wait(&sem_black);  // BLACK waits for turn
-        }
-
-        memset(buffer, 0, 1024);
-        int valread = read(player_socket, buffer, 1024);
-        if (valread <= 0) break;
-
-        {
-            std::lock_guard<std::mutex> lock(cout_mutex);
-            std::cout << "[Match Thread][" << color << "] Received: " << buffer << std::endl;
-        }
-
-        send(opponent_socket, buffer, strlen(buffer), 0);
-
-        // Give turn to other player
-        if (color == "WHITE") {
-            sem_post(&sem_black);
-        } else {
-            sem_post(&sem_white);
-        }
-    }
-
-    {
-        std::lock_guard<std::mutex> lock(cout_mutex);
-        std::cout << "[Match Thread][" << color << "] Player disconnected. Ending match." << std::endl;
-    }
-
-    close(player_socket);
-    close(opponent_socket);
-    exit(0);  // Ends the child process cleanly
-}
-
-void sigchld_handler(int signum) {
+void handle_sigchld(int) {
+    // Cleanup finished child processes to avoid zombies
     while (waitpid(-1, nullptr, WNOHANG) > 0);
 }
 
-int main() {
-    signal(SIGCHLD, sigchld_handler);
+void match_handler(int client1, int client2) {
+    std::cout << "[Server] Starting new match: " << client1 << " vs " << client2 << "\n";
 
-    int server_fd;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
+    send(client1, "ROLE:WHITE", strlen("ROLE:WHITE"), 0);
+    send(client2, "ROLE:BLACK", strlen("ROLE:BLACK"), 0);
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == 0) {
-        perror("socket failed");
-        return 1;
+    fd_set readfds;
+    char buffer[1024];
+
+    while (true) {
+        FD_ZERO(&readfds);
+        FD_SET(client1, &readfds);
+        FD_SET(client2, &readfds);
+        int max_sd = std::max(client1, client2) + 1;
+
+        int activity = select(max_sd, &readfds, nullptr, nullptr, nullptr);
+        if (activity < 0) continue;
+
+        for (int i = 0; i < 2; ++i) {
+            int sender = (i == 0) ? client1 : client2;
+            int receiver = (i == 0) ? client2 : client1;
+
+            if (FD_ISSET(sender, &readfds)) {
+                int bytes = recv(sender, buffer, sizeof(buffer), 0);
+                if (bytes <= 0) {
+                    std::cout << "[Match] Player disconnected. Ending match.\n";
+                    close(client1);
+                    close(client2);
+                    return;
+                }
+                send(receiver, buffer, bytes, 0);
+            }
+        }
     }
+}
 
-    int opt = 1;
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+void cleaner(bool wait_for_cleanup) {
+    if (wait_for_cleanup) sleep(1);
+    std::lock_guard<std::mutex> lock(pid_mutex);
+    for (pid_t pid : child_pids) {
+        int status;
+        waitpid(pid, &status, 0);
+    }
+    child_pids.clear();
+}
 
+int main() {
+    signal(SIGCHLD, handle_sigchld);
+    sem_init(&connection_sem, 0, 1);
+
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in address{};
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        return 1;
-    }
-
+    bind(server_fd, (sockaddr*)&address, sizeof(address));
     listen(server_fd, 10);
-    std::cout << "Server ready. Waiting for players..." << std::endl;
-
-    std::vector<int> waiting_players;
-    int match_id = 1;
+    std::cout << "[Server] Listening on port " << PORT << "...\n";
 
     while (true) {
-        int new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-        if (new_socket < 0) {
-            perror("accept");
-            continue;
-        }
+        sem_wait(&connection_sem);
 
-        {
-            std::lock_guard<std::mutex> lock(cout_mutex);
-            std::cout << "New player connected: socket " << new_socket << std::endl;
-        }
+        int client1 = accept(server_fd, nullptr, nullptr);
+        std::cout << "[Server] New player connected: socket " << client1 << "\n";
 
-        waiting_players.push_back(new_socket);
+        int client2 = accept(server_fd, nullptr, nullptr);
+        std::cout << "[Server] New player connected: socket " << client2 << "\n";
 
-        if (waiting_players.size() >= 2) {
-            int p1 = waiting_players[0];
-            int p2 = waiting_players[1];
-            waiting_players.erase(waiting_players.begin(), waiting_players.begin() + 2);
-
-            pid_t pid = fork();
-            if (pid == 0) {
-                // Child process handles this match
-                sem_init(&sem_white, 0, 1); // WHITE starts first
-                sem_init(&sem_black, 0, 0); // BLACK waits
-
-                std::thread t1(handle_player, p1, p2, "WHITE");
-                std::thread t2(handle_player, p2, p1, "BLACK");
-
-                t1.join();
-                t2.join();
-
-                sem_destroy(&sem_white);
-                sem_destroy(&sem_black);
-                exit(0);
-            } else if (pid > 0) {
-                // Parent closes unused sockets
-                close(p1);
-                close(p2);
-                match_id++;
-            } else {
-                perror("fork failed");
+        pid_t pid = fork();
+        if (pid == 0) {
+            close(server_fd);
+            match_handler(client1, client2);
+            exit(0);
+        } else {
+            close(client1);
+            close(client2);
+            {
+                std::lock_guard<std::mutex> lock(pid_mutex);
+                child_pids.push_back(pid);
             }
+            std::thread(cleaner, true).detach();
         }
+
+        sem_post(&connection_sem);
     }
 
-    close(server_fd);
+    sem_destroy(&connection_sem);
     return 0;
 }
-
